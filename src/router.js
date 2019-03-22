8106b3090e58d7f4b89fa9d20cb4f1f12a22f2ab
missing-star <1652575963@qq.com>
2019-03-22 09:28:24 +0800

    loading

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Loading from './views/Loading.vue'
 
 Vue.use(Router)
 
@@ -10,8 +11,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      name: 'loading',
+      component: Loading
+    },
+    {
+      path:'/home',
+      name:'home',
+      component:Home
     }
   ]
 })
