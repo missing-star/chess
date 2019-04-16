4b4010697b37d9018691dfa86c4f7fe54f572781
421538074 <421538074@qq.com>
2019-04-16 17:32:06 +0800

    新增弹框

---
+++ src/components/WaterBox.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="water-box">
+    <div class="chess-dialog-body water-box-wrap" :class="{'hide':!isShow}">
+      <img src="../assets/images/close.png" class="mail-box-close" @click="closeMyself">
+
+      <img src="../assets/images/water-1.png" class="water-img" alt>
+
+      <div class="water-box-foot">
+        <img src="../assets/images/弹框-再玩一次.png" alt>
+        <img src="../assets/images/弹框-下一关.png" alt>
+      </div>
+    </div>
+    <chess-mask :is-show="isShow"></chess-mask>
+  </div>
+</template>
+<script>
+import ChessMask from "./Mask";
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    closeMyself() {
+      this.$emit("hide");
+    }
+  },
+  props: ["is-show"],
+  components: {
+    [ChessMask.name]: ChessMask
+  }
+};
+</script>
+<style scoped>
+.water-box-wrap {
+  background: url(../assets/images/弹框-喷水.png) no-repeat;
+  background-size: 100% 100%;
+}
+
+img.mail-box-close {
+  right: -3rem;
+  top: 8rem;
+}
+
+img.water-img {
+  width: 67%;
+  position: absolute;
+  top: 57%;
+  left: 16%;
+}
+
+.water-box-foot {
+  position: absolute;
+  bottom: 10%;
+  left: 18%;
+}
+.water-box-foot img {
+  width: 24%;
+  margin: 0 10%;
+  cursor: pointer;
+}
+</style>
