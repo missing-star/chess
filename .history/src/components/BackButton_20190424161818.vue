9e3c3462e4e8d8d3db5467efdb42a4ae76b0b9bf
missing-star <1652575963@qq.com>
2019-04-24 16:24:37 +0800

    ok

---
+++ .history/src/components/BackButton_20190424161818.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="back-btn-wrapper">
+    <img @click="goBack" src="../assets/images/back.png" class="back-icon pointer">
+  </div>
+</template>
+<script>
+export default {
+  name: "chess-back-button",
+  methods: {
+    goback() {
+      // this.$emit("go-back");
+      console.log('router='+this.$router)
+      // this.$router.back(-1);
+    }
+  }
+};
+</script>
+<style scoped>
+div.back-btn-wrapper {
+  position: absolute;
+  bottom: 1rem;
+  left: 1rem;
+  width: 5rem;
+}
+img.back-icon {
+  width: 100%;
+}
+</style>
---
+++ .history/src/components/BackButton_20190424162250.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="back-btn-wrapper">
+    <img @click="goBack" src="../assets/images/back.png" class="back-icon pointer">
+  </div>
+</template>
+<script>
+export default {
+  name: "chess-back-button",
+  methods: {
+    goBack() {
+      console.log(this.$route)
+      if(this.$route.path == '/online-race') {
+        this.$emit('quit-game');
+      }
+      else {
+        this.$router.back(-1);
+      }
+    },
+  }
+};
+</script>
+<style scoped>
+div.back-btn-wrapper {
+  position: absolute;
+  bottom: 1rem;
+  left: 1rem;
+  width: 5rem;
+}
+img.back-icon {
+  width: 100%;
+}
+</style>
