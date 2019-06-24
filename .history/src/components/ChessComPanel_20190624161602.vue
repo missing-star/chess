b7f003f564d46215e3e704d0d04e62f54370a1e4
missing-star <1652575963@qq.com>
2019-06-24 16:42:50 +0800

    背景音乐

---
+++ .history/src/components/ChessComPanel_20190624161602.vue
@@ -0,0 +1,236 @@
+<template>
+  <div class="chess-com-panel">
+    <!-- 背景音乐 -->
+    <audio ref="comBg" loop preload="auto" muted>
+      <source src="../assets/audio/我的棋社作业背景.mp3">
+    </audio>
+    <div class="chess-dialog-body chess-com-panel-body" :class="{'hide':!isShow}">
+      <img src="../assets/images/close.png" class="mail-box-close" @click="closeMyself">
+      <div class="content-wrapper-container">
+        <div class="category-wrapper">
+          <div @click="openPanel(item.id)" v-for="item in categoryList" :key="item.id" class="category-item pointer">
+            <img :src="item.icon" class="category-item icon">
+          </div>
+        </div>
+        <div class="userinfo-wrapper">
+          <img :src="information.picture | filterImg" class="user-logo">
+          <p class="nickname">{{information.nickname}}</p>
+          <div class="exp-wrapper">
+            <p class="exp-title">经验值</p>
+            <div class="exp-progressbar-wrapper">
+              <div class="exp-progressbar"
+                :style="information.experience/information.total_experience*100+'%' | filterWidth"></div>
+            </div>
+            <span class="exp-value">{{information.experience}}/{{information.total_experience}}</span>
+          </div>
+          <div class="userinfo-detail-wrapper">
+            <p class="userinfo-item inline">棋力等级：{{information.grade_name}}</p>
+            <p class="userinfo-item inline">师傅：{{information.master_name?information.master_name:"暂无师傅"}}</p>
+            <p class="userinfo-item inline">班级：{{information.class_nickname}}</p>
+            <p class="userinfo-item inline">性别：{{information.sex==1?"男":"女"}}</p>
+            <p class="userinfo-item block">老师：{{information.teacher_name}}</p>
+            <p class="userinfo-item block">学校：{{information.mechanism_name}}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <chess-mask :is-show="isShow"></chess-mask>
+  </div>
+</template>
+<script>
+  import ChessMask from "./Mask";
+  export default {
+    name: "chess-com-panel",
+    props: ["is-show", "information"],
+    data() {
+      return {
+        categoryList: [{
+            id: 0,
+            icon: require("../assets/images/成长日志.png")
+          },
+          {
+            id: 1,
+            icon: require("../assets/images/我的成就.png")
+          },
+          {
+            id: 2,
+            icon: require("../assets/images/我的作业.png")
+          },
+          {
+            id: 3,
+            icon: require("../assets/images/自习室.png")
+          }
+        ]
+      };
+    },
+    components: {
+      [ChessMask.name]: ChessMask
+    },
+    methods: {
+      closeMyself() {
+        this.$emit("hide");
+        this.pauseMusic();
+      },
+      openPanel(target) {
+        switch (target) {
+          case 0:
+            this.closeMyself();
+            this.$emit("open-log");
+            break;
+          case 1:
+            this.closeMyself();
+            this.$emit("open-ach");
+            break;
+            break;
+          case 2:
+            this.closeMyself();
+            this.$emit("open-homework");
+            break;
+            break;
+          case 3:
+            this.closeMyself();
+            this.$emit("open-self");
+            break;
+        }
+      },
+      playMusic() {
+        this.$nextTick(() => {
+          this.$refs.comBg.addEventListener('canplay', () => {
+            this.$refs.comBg.play();
+          });
+        });
+      },
+      pauseMusic() {
+        this.$refs.comBg.pause();
+      }
+    },
+    watch:{
+      isShow() {
+        if(this.isShow) {
+          this.playMusic();
+        }
+      }
+    },
+    filters: {
+      filterWidth(chartWidth) {
+        if (chartWidth == "0%") {
+          chartWidth = "0%";
+        }
+        return {
+          width: `${chartWidth}`
+        };
+      }
+    },
+  };
+</script>
+<style scoped>
+  div.chess-com-panel-body {
+    background: url(../assets/images/community-bg.png) no-repeat;
+    background-size: 100% 100%;
+  }
+
+  div.content-wrapper-container {
+    width: 82%;
+    height: 80%;
+    margin-top: 10%;
+    margin-left: 8%;
+    display: flex;
+    align-items: center;
+  }
+
+  .category-wrapper {
+    width: 40%;
+    height: 85%;
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+    justify-content: space-between;
+  }
+
+  .category-item {
+    width: 46%;
+  }
+
+  img.category-item.icon {
+    width: 100%;
+    height: 90%;
+  }
+
+  .userinfo-wrapper {
+    width: 60%;
+    padding-left: 10%;
+    height: 100%;
+    padding-top: 2%;
+    text-align: center;
+  }
+
+  img.user-logo {
+    width: 20%;
+    max-height: 18%;
+    border-radius: 50%;
+    background: skyblue;
+  }
+
+  p.nickname {
+    width: 40%;
+    background: #c38e72;
+    padding: 0.5rem;
+    border-radius: 0.2rem;
+    color: #fff;
+    margin: 1rem auto;
+  }
+
+  .exp-progressbar-wrapper {
+    width: 60%;
+    height: 0.8rem;
+    background: #fff;
+    border-radius: 1rem;
+  }
+
+  .exp-progressbar {
+    height: 100%;
+    width: auto;
+    background: #ff7226;
+    border-radius: 1rem;
+  }
+
+  .exp-wrapper {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    color: #b87f61;
+    height: 5%;
+  }
+
+  p.userinfo-item {
+    background: #fcf3d6;
+    border-radius: 0.2rem;
+    margin: 2.2% 0;
+    display: inline-flex;
+    align-items: center;
+    color: #b87f61;
+    padding: 0.5rem;
+    font-size: 0.9rem;
+  }
+
+  p.userinfo-item {
+    height: 18%;
+  }
+
+  p.userinfo-item.inline {
+    width: 45%;
+  }
+
+  p.userinfo-item.block {
+    width: 100%;
+  }
+
+  .userinfo-detail-wrapper {
+    margin-top: 1%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    height: 50%;
+  }
+</style>
---
+++ .history/src/views/StarRoom_20190624151719.vue
@@ -0,0 +1,423 @@
+<template>
+  <div class="chess-star-wrapper">
+    <!-- 背景音乐 -->
+		<audio ref="starBg">
+			<source src="../assets/audio/将星阁背景音乐.mp3">
+		</audio>
+    <!-- 老师信息 -->
+    <div class="inform">
+      <div class="inform_left">
+        <img
+          :src="teacherInfo.picture == null ? require('../assets/images/user-logo.png') : teacherInfo.picture"
+          alt
+        >
+      </div>
+      <div class="inform_right">
+        <ul class="inform_right_uu">
+          <li>姓名：{{teacherInfo.admin_name}}</li>
+          <li>性别：{{teacherInfo.sex == 1 ? '男' : '女'}}</li>
+          <li>年龄：{{teacherInfo.age}}</li>
+          <li>所属学校：{{teacherInfo.mechanism_name}}</li>
+        </ul>
+        <div class="inform_right_introduce">
+          <p>简介:</p>
+          <p>{{teacherInfo.describe}}</p>
+          <p @click="openTeacherDetail" class="detail-link">详情</p>
+        </div>
+      </div>
+      <div class="obeying_master" @click="Vteacher"></div>
+    </div>
+    <div class="teacher-list-wrapper">
+      <div class="pre-wrapper swiper-button-prev">
+        <img v-if="isFirstPage" src="../assets/images/arrow-left-disabled.png" class="page-icon">
+        <img v-else src="../assets/images/arrow-left-big.png" class="page-icon">
+      </div>
+      <div class="next-wrapper swiper-button-next">
+        <img v-if="isLastPage" src="../assets/images/arrow-right-disabled.png" class="page-icon">
+        <img v-else src="../assets/images/arrow-right-big.png" class="page-icon">
+      </div>
+      <div class="swiper-container swiper-no-swiping">
+        <div class="content-wrapper-container swiper-wrapper">
+          <div class="swiper-slide" v-for="n in Math.ceil(teacherList.length / 7)" :key="n">
+            <div
+              @click="selectTeacher(teacher.id)"
+              class="teacher-item pointer"
+              :class="{active:currentId == teacher.id}"
+              v-for="teacher in teacherList.slice(7*(n-1),7*(n-1)+7)"
+              :key="teacher.id"
+            >
+              <img
+                :src="teacher.picture == null ? require('../assets/images/user-logo.png') : teacher.picture"
+                class="teacher-logo"
+              >
+
+              <p class="teacher-name">{{teacher.admin_name}}</p>
+            </div>
+          </div>
+        </div>
+        <div class="swiper-pagination"></div>
+      </div>
+    </div>
+    <chess-back-button></chess-back-button>
+    <!-- 拜师成功 -->
+    <create-sucess
+      :is-show="showCreateSucess"
+      :avter="avter"
+      :btnImg="btnImg"
+      :show1="show1"
+      :show="show"
+    ></create-sucess>
+    <!-- 教师详情 -->
+    <chess-teacher-detail-panel @hide="closeTeacherDetail" :content="currentDetail" :is-show="showTeacherDetail"></chess-teacher-detail-panel>
+  </div>
+</template>
+<script>
+import BackButton from "../components/BackButton";
+import CreateSucess from "../components/CreateSucess";
+import TeacherDetail from "../components/TeacherDetail";
+import Swiper from "swiper";
+export default {
+  data() {
+    return {
+      teacherList: [],
+      isFirstPage: true,
+      isLastPage: false,
+      showCreateSucess: false, //成功
+      currentId: "",
+      teacherInfo: "",
+      avter: "",
+      btnImg: "",
+      show1: false,
+      show: true,
+      showTeacherDetail:false,
+      currentDetail:''
+    };
+  },
+  components: {
+    [BackButton.name]: BackButton,
+    CreateSucess,
+    [TeacherDetail.name]:TeacherDetail
+  },
+  methods: {
+    isClose() {
+      //提示框消失
+      setTimeout(() => {
+        this.showCreateSucess = false;
+      }, 1500);
+    },
+    computedLength() {
+      return Math.ceil(this.teacherList.length / 7) > 1;
+    },
+    openTeacherDetail() {
+      this.showTeacherDetail = true;
+    },
+    closeTeacherDetail() {
+      this.showTeacherDetail = false;
+    },
+    selectTeacher(id) {
+      if (this.currentId != id) {
+        // this.getTeacherDetail(id);
+        this.currentId = id;
+        this.$axios({
+          url: `${
+            process.env.VUE_APP_URL
+          }index.php?r=api-teach/select-teach-detail`,
+          method: "post",
+          data: this.qs.stringify({
+            teach_id: id
+          })
+        })
+          .then(res => {
+            this.teacherInfo = res.data.data;
+            this.currentDetail = this.teacherInfo.describe;
+          })
+          .catch(err => {
+            alert("服务器异常");
+          });
+      }
+      sessionStorage.setItem("teachId", id);
+    },
+    getTeacherList() {
+      this.$axios({
+        url: `${
+          process.env.VUE_APP_URL
+        }index.php?r=api-teach/select-teach-lists`,
+        method: "post"
+      })
+        .then(res => {
+          if (res.data.status == 1) {
+            this.teacherList = res.data.data;
+            this.getTeacherDetail(this.teacherList[0].id);
+          }
+        })
+        .catch(err => {});
+    },
+    getTeacherDetail(id) {
+      this.currentId = id;
+      this.$axios({
+        url: `${
+          process.env.VUE_APP_URL
+        }index.php?r=api-teach/select-teach-detail`,
+        method: "post",
+        data: this.qs.stringify({
+          teach_id: id
+        })
+      })
+        .then(res => {
+          this.teacherInfo = res.data.data;
+          this.currentDetail = this.teacherInfo.describe;
+          this.initSwiper();
+        })
+        .catch(err => {
+          alert("服务器异常");
+        });
+    },
+    initSwiper() {
+      this.isLastPage =
+        Math.ceil(this.teacherList.length / 7) > 1 ? false : true;
+      this.$nextTick(() => {
+        const vm = this;
+        new Swiper(".swiper-container", {
+          watchSlidesProgress: true,
+          pagination: {
+            el: ".swiper-pagination"
+          },
+          navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev"
+          },
+          on: {
+            progress: function(progress) {
+              if (this.progress == 0) {
+                //无法点击上一页
+                vm.isFirstPage = true;
+                if (vm.computedLength()) {
+                  vm.isLastPage = false;
+                }
+              }
+              if (this.progress == 1) {
+                //无法点击最后一页
+                vm.isLastPage = true;
+                vm.isFirstPage = false;
+              }
+            }
+          }
+        });
+      });
+    },
+    //拜师
+    Vteacher() {
+      var teacherId = sessionStorage.getItem("teachId");
+      if (sessionStorage.getItem("teachId")) {
+        var teacherId = sessionStorage.getItem("teachId");
+      } else {
+        teacherId = this.teacherList[0].id;
+      }
+      this.$axios({
+        url: `${
+          process.env.VUE_APP_URL
+        }/index.php?r=api-student/apprentice-teacher`,
+        method: "post",
+        data: this.qs.stringify({
+          teach_id: teacherId
+        })
+      })
+        .then(res => {
+          if (res.data.status == 1) {
+            this.avter = require("../assets/images/拜师成功.png");
+            this.showCreateSucess = true;
+            this.isClose();
+          } else {
+            this.avter = require("../assets/images/你已拜师.png");
+            this.showCreateSucess = true;
+            this.isClose();
+            // alert("你已拜师");
+          }
+        })
+        .catch(err => {
+          alert("服务器异常");
+        });
+    }
+  },
+  mounted() {
+    this.$nextTick(() => {
+      console.log('开始')
+      this.$refs.starBg.canplay = (() => {
+        this.$refs.starBg.play();
+      });
+    });
+    this.getTeacherList();
+  }
+};
+</script>
+<style scoped>
+
+div.chess-star-wrapper {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  background: url(../assets/images/将星阁banner.jpg) no-repeat;
+  background-size: 100% 100%;
+}
+div.inform {
+  width: 35%;
+  height: 50%;
+  display: flex;
+  align-items: center;
+  position: absolute;
+  top: 16%;
+  left: 33%;
+}
+div.inform_left {
+  width: 45%;
+  height: 73%;
+  background: url(../assets/images/picture.png) no-repeat;
+  background-size: 100% 100%;
+  margin-right: 7%;
+  padding: 4% 3%;
+}
+div.inform_left img {
+  width: 100%;
+  height: 100%;
+}
+div.inform_right {
+  width: 48%;
+  height: 65%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+}
+ul.inform_right_uu li {
+  font-family: PingFangSC-Semibold;
+  font-size: 1.2rem;
+  color: #65400a;
+  text-align: left;
+  line-height: 1.5rem;
+}
+div.inform_right_introduce {
+  width: 100%;
+  height: 66%;
+  background: url(../assets/images/information.png) no-repeat;
+  background-size: 100% 100%;
+  padding: 10% 7%;
+}
+
+div.inform_right_introduce p {
+  font-family: PingFangSC-Semibold;
+  font-size: 1.3rem;
+  color: #65400a;
+  text-align: left;
+  margin-bottom: 2%;
+}
+
+div.inform_right_introduce p:nth-child(2) {
+  font-family: PingFangSC-Regular;
+  font-size: 1rem;
+  color: #65400a;
+  text-align: left;
+  line-height: 1.3rem;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 6;
+  overflow: hidden;
+}
+
+div.obeying_master {
+  width: 26%;
+  height: 20%;
+  background: url(../assets/images/拜师.png) no-repeat;
+  background-size: 100% 100%;
+  position: absolute;
+  bottom: -10%;
+  left: 37%;
+  cursor: pointer;
+}
+.teacher-list-wrapper {
+  width: 58%;
+  top: 72%;
+  left: 21%;
+  position: absolute;
+  height: 20%;
+}
+.swiper-slide {
+  display: flex;
+  align-items: flex-end;
+  justify-content: flex-start;
+  background: transparent;
+}
+.teacher-item {
+  background: url(../assets/images/teacher-bg.png) no-repeat;
+  background-size: 100% 100%;
+  height: 85%;
+  width: calc(100% / 8);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  max-width: 130px;
+  margin: 0 1.2%;
+}
+.teacher-item.active {
+  background: url(../assets/images/teacher-bg-active.png) no-repeat;
+  background-size: 100% 100%;
+  height: 85%;
+  width: calc(100% / 8);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  max-width: 130px;
+  position: relative;
+  top: -10%;
+}
+.pre-wrapper.swiper-button-prev,
+.next-wrapper.swiper-button-next {
+  position: absolute;
+  top: 0 !important;
+  width: 3%;
+}
+@media screen and (min-width: 1400px){
+  .pre-wrapper.swiper-button-prev,
+  .next-wrapper.swiper-button-next {
+    position: absolute;
+    top: -4% !important;
+    width: 3%;
+  }
+}
+.pre-wrapper.swiper-button-prev {
+  left: 20%;
+}
+.next-wrapper.swiper-button-next {
+  right: 20%;
+}
+img.page-icon {
+  width: 100%;
+}
+img.teacher-logo {
+  background: #fff;
+  width: 75%;
+  max-height: 65%;
+  border-radius: 0.1rem;
+}
+.teacher-item p {
+  color: #fff;
+  font-size: 0.8rem;
+}
+.teacher-item.active p {
+  color: #7e4f26;
+}
+.swiper-pagination.swiper-pagination-bullets {
+  display: none;
+}
+p.detail-link {
+    text-decoration: underline;
+    text-align: right !important;
+    display: block;
+    font-size: 0.8rem !important;
+    cursor: pointer;
+}
+.swiper-button-next, .swiper-button-prev{
+  z-index: 3;
+}
+</style>
